471d487526ccfcbbb1ed9d2086ba8359b95b603f
Jenni-MS <69949315+Jenni-MS@users.noreply.github.com>
2026-06-21 23:05:39 -0600

    Update cors.php

--- proyecto2-api/config/cors.php
+++ proyecto2-api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://frontendproyecto3.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
